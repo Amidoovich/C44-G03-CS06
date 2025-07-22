d9d059858c5fa3b43f2846a219170c1d3c98ecae
Amidooovich <144342730+Amidoovich@users.noreply.github.com>
2025-07-22 18:00:03 +0300

    Vid02

--- S6/Program.cs
+++ S6/Program.cs
@@ -4,7 +4,45 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Array
+            #region 1D Array
+            #region Example 01
+            //int[] Numbers;
+            //Numbers = new int[3];
+
+            //Numbers[0] = 1;
+            //Numbers[1] = 2;
+            //Numbers[2] = 3;
+
+            //Console.WriteLine(Numbers[0]);
+            //Console.WriteLine(Numbers[1]);
+            //Console.WriteLine(Numbers[2]);
+
+
+            //Console.WriteLine(Numbers.Length);
+            //Console.WriteLine(Numbers.Rank);
+
+            //for (int i = 0; i < 3; i++) 
+            //{
+            //    Console.WriteLine(Numbers[i]);
+            //}
+
+            //foreach (int i in Numbers) 
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+
+            #endregion
+            #region Array Creation Ways
+            //int[] Numbers01 = new int[3];
+            //int[] Numbers02 =  new int[3] { 1, 2, 3} ;
+            //int[] Numbers03 =  new int[] { 1, 2, 3} ;
+            //int[] Numbers04 = { 1, 2, 3} ;
+            #endregion
+
+            #endregion
+            #endregion
         }
     }
 }
